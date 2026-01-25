1decb102bc54fe5c11379cf9991d34821558451b
DebellareSuperbos <temastar771@gmail.com>
2026-01-25 12:54:34 +0300

    PZ6.3.py

--- PZ6/PZ6.3.py
+++ PZ6/PZ6.3.py
@@ -1,16 +1,23 @@
 # Дан список размера N, все элементы которого, кроме последнего, упорядочены по возрастанию. Сделать список упорядоченным, переместив последний элемент на новую позицию.
 
-print("\nЗадача 3")
-A = [1, 3, 5, 7, 2] 
-print("Список:", A)
-
-if len(A) > 1:
-    last = A[-1]
-    pos = len(A) - 1
-    i = len(A) - 2
-    while i >= 0 and A[i] > last:
-        A[i+1] = A[i]
-        pos = i
-        i -= 1
-    A[pos] = last
-print("Результат:", A)
+print("\nЗадание 3")
+try:
+    N = int(input("Размер списка: "))
+    if N <= 2:
+        print("Список слишком мал")
+    else:
+        A = sorted([random.randint(1, 50) for _ in range(N-1)])
+        A.append(random.randint(1, 50))
+        print("Исходный:", A)
+        
+        last = A[-1]
+        i = N - 2
+        while i >= 0 and A[i] > last:
+            A[i+1] = A[i]
+            i -= 1
+        A[i+1] = last
+        
+        print("Упорядоченный:", A)
+        
+except ValueError:
+    print("Ошибка ввода")
