5561d81075627bf021fc7ddf9492002250a33ea1
DebellareSuperbos <temastar771@gmail.com>
2026-03-13 18:34:29 +0300

    PZ_2.py

--- PZ8/PZ_2.py
+++ PZ8/PZ_2.py
@@ -1,1 +1,17 @@
+height_dict = {
+    "Андрей": 178,
+    "Виктор": 150,
+    "Максим": 200,
+    "Елена": 165,
+    "Ольга": 158,
+    "Дмитрий": 182
+}
+print("Словарь роста персон:")
+for name, height in height_dict.items():
+    print(name + ":", height, "см.")
 
+max_height = max(height_dict.values())
+min_height = min(height_dict.values())
+
+print("\nНаибольший рост:", max_height, "см.")
+print("Наименьший рост:", min_height, "см.")
