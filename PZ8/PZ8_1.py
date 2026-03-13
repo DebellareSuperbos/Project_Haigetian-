7e04bca48f6cfd611d5e64c20c58c67dd8a48029
DebellareSuperbos <temastar771@gmail.com>
2026-03-13 18:35:47 +0300

    PZ8_1.py

--- PZ8/PZ8_1.py
+++ PZ8/PZ8_1.py
@@ -1,1 +1,11 @@
+#Найдите ключ с минимальным значением в sample_dict = {'Physics': 82, 'Math': 65,'history': 75}.
 
+
+sample_dict = {'Physics': 82, 'Math': 65, 'history': 75}
+
+print("Исх словарь:", sample_dict)
+
+min_key = min(sample_dict, key=sample_dict.get)
+
+print("Ключ с минимальным значением:", min_key)
+print("Значение:", sample_dict[min_key])
