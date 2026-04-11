2b4643a8c66b48fba8c77ca2c4f101fa7c90c64e
DebellareSuperbos <temastar771@gmail.com>
2026-04-11 10:41:13 +0300

    PZ11_2.py

--- PZ11/PZ11_2.py
+++ PZ11/PZ11_2.py
@@ -1,1 +1,9 @@
+#Из заданной строки отобразить только символы нижнего регистра.
+#Использовать библиотеку string. Строка 'In PyCharm, you can specify third-party standalone
+#applications and run them as External Tools'.
 
+import string
+
+s = 'In PyCharm, you can specify third-party standalone applications and run them as External Tools'
+low = [c for c in s if c in string.ascii_lowercase]
+print(''.join(low))
