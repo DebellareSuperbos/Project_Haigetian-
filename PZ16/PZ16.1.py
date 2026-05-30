0a04f82497b22cd7d55a7de172b5dc7b37fa3592
DebellareSuperbos <temastar771@gmail.com>
2026-05-30 16:12:44 +0300

    PZ16.1.py

---
+++ PZ16/PZ16.1.py
@@ -0,0 +1,20 @@
+# Класс "Книга"
+class Book:
+    def __init__(self, title, author, pages):
+        self.title = title
+        self.author = author
+        self.pages = pages
+
+    def read(self):
+        print(f"Читаем книгу: {self.title}")
+
+    def write(self, new_title, new_author):
+        self.title = new_title
+        self.author = new_author
+        print(f"Книга перезаписана! Теперь это '{self.title}' автора {self.author}")
+
+# Тестирование
+b = Book("Война и мир", "Толстой", 1200)
+b.read()
+b.write("Преступление и наказание", "Достоевский")
+print(f"Название: {b.title}, Автор: {b.author}, Страниц: {b.pages}")
